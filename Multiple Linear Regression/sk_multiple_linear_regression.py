ab5908623f029a75b6954f28a9701808d5a8627c
sk-falcon <sparsh.dtu@gmail.com>
2017-08-06 14:42:15 +0530

    Multiple Linear Regression

---
+++ Multiple Linear Regression/sk_multiple_linear_regression.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 15:21:06 2017
+
+@author: sparshkoyarala
+"""
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+dataset = pd.read_csv('50_Startups.csv')
